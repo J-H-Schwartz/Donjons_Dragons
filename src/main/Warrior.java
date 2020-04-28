b15f40f87123df2f228f6d51b19ac0b64c78f1b3
Jonathan <schwartz.jo@laposte.net>
2020-04-28 12:18:58 +0200

    First commit

---
+++ src/main/Warrior.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Warrior {
+	public String name;
+	public int life;
+	public int attack_power;
+	public String weapon;
+	public String shield;
+}
---
+++ src/main/Wizard.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Wizard {
+	public String name;
+	public int life;
+	public int attack_power;
+	public String spell;
+	public String potion;
+}
